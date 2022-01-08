b919af9b4631baff73a4884981c243b5c70897dd
Loicyeu <loic.henry2001@gmail.com>
2022-01-08 23:12:12 +0100

    Add ratio Users/Diff/Bin

--- models/ratios.py
+++ models/ratios.py
@@ -13,4 +13,5 @@ sorting_ratios: list[tuple[RatioLambda, str]] = [
     (lambda f, c: [(f, f.daily_users / len(f.get_binaries())) for f in f], "Users/Bin"),
     (lambda f, c: [(f, f.daily_users / len(f.services)) for f in f], "Users/Services"),
     (lambda f, c: [(f, f.daily_users / f.difficulty / len(f.services)) for f in f], "Users/Diff/Services"),
+    (lambda f, c: [(f, f.daily_users / f.difficulty / len(f.get_binaries())) for f in f], "Users/Diff/Bin"),
 ]
